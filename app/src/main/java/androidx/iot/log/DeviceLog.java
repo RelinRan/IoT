a445c05198a83bad36600ef9fd0795ae699e51ff
relinran <1101005304@qq.com>
2024-07-25 11:23:39 +0800

    update

--- app/src/main/java/androidx/iot/log/DeviceLog.java
+++ app/src/main/java/androidx/iot/log/DeviceLog.java
@@ -18,48 +18,50 @@ import java.util.Date;
  */
 public class DeviceLog extends LogFile {
 
-    private final String TAG = DeviceLog.class.getSimpleName();
-    private SimpleDateFormat format;
+    private static DeviceLog instance;
 
-    public DeviceLog(Context context) {
+    public static DeviceLog initialize(Context context, String projectDir) {
+        if (instance == null) {
+            synchronized (DeviceLog.class) {
+                if (instance == null) {
+                    instance = new DeviceLog(context.getApplicationContext());
+                    instance.setFolder(projectDir, "Device");
+                    instance.setPrefix("device");
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static DeviceLog acquire() {
+        return instance;
+    }
+
+    private DeviceLog(Context context) {
         super(context);
     }
 
     @Override
     protected void initializeParameters(Context context, String project, String dir, String prefix) {
         super.initializeParameters(context, project, dir, prefix);
-        format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         setSupportScheduled(false);
     }
 
     /**
-     * 保存
+     * 日志
+     *
+     * @param content 内容
      */
-    public void save() {
+    public void log(String... content) {
+        if (instance == null) {
+            return;
+        }
         StringBuilder builder = new StringBuilder();
-        builder.append("Application:" + Apk.getApplicationName(getContext()) + "\n");
-        builder.append("Date:" + format.format(new Date()) + "\n");
-        builder.append("Version Name:" + Apk.getVersionName(getContext()) + "\n");
-        builder.append("Version Code:" + Apk.getVersionCode(getContext()) + "\n");
-        builder.append("License:" + License.acquire().isLicensed() + "\n");
-        builder.append("SN:" + Device.getUniqueId(getContext()) + "\n");
-        builder.append("WIFI IP:" + Device.getWifiIpAddress(getContext()) + "\n");
-        //分辨率
-        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
-        int screenWidth = displayMetrics.widthPixels;
-        int screenHeight = displayMetrics.heightPixels;
-        builder.append("Display:" + screenWidth + " * " + screenHeight + "\n");
-        //屏幕密度
-        float density = getContext().getResources().getDisplayMetrics().density;
-        builder.append("Density:" + density + "\n");
-        //内存信息
-        Memory memory = new Memory(getContext());
-        ActivityManager.MemoryInfo memoryInfo = memory.getMemoryInfo();
-        builder.append("TotalMem:" + memory.formatMemorySize(memoryInfo.totalMem) + "\n");
-        builder.append("AvailMem:" + memory.formatMemorySize(memoryInfo.availMem) + "\n");
-        builder.append("LowMemory:" + memoryInfo.lowMemory + "\n");
-        Console.i(TAG, builder.toString());
-        write(builder.toString(), false);
+        for (String item : content) {
+            builder.append(item);
+        }
+        String value = builder.toString();
+        write(value);
     }
 
 }
